4085c431fdaf62dca2eb2592d85206eed1802a50
Andrianov Konstantsin <andrianov.dev@yandex.by>
2016-05-17 12:34:07 +0300

    +presets setting to makefie

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 FILE='./../../Makefile'
-DATA="# SHAFT-BUILD \nBUILD_SRC = 'src'\nBUILD_LIB = 'lib'\ninclude ./node_modules/shaft-build/Makefile\n# /SHAFT-BUILD"
+DATA="# SHAFT-BUILD \nBUILD_SRC = 'src'\nBUILD_LIB = 'lib'\nPRESETS = '--presets=node5,stage-0'\ninclude ./node_modules/shaft-build/Makefile\n# /SHAFT-BUILD"
 
 if test -e $FILE && grep -q SHAFT-BUILD $FILE; then
 	sed -i "/SHAFT-BUILD/,/\/SHAFT-BUILD/c\\\n${DATA}" $FILE
 else
-	printf "\n${DATA}" >> $FILE 
+	printf "\n${DATA}" >> $FILE
 fi
